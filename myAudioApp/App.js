eaa6e5b75d8ebdcc179d6eedbe2bc6481ac93e6f
markocdroid <markoc.dev@gmail.com>
2026-04-10 16:06:35 -0400

    Finished the practice exercise

--- myAudioApp/App.js
+++ myAudioApp/App.js
@@ -2,17 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, TouchableOpacity, Text, View, Image } from 'react-native';
 import { useState } from 'react';
 import { Audio } from 'expo-av';
+import { Picker } from 'react-native-web';
+
+const sound1 = require('./assets/sampleAudioForReactnative.m4a');
+const sound2 = require('./assets/Diviners_Ren_ Savannah_2026_Japanese_Version_NCS.m4a');
 
 export default function App() {
     const [isPlaying, setIsPlaying] = useState(false);
     const [sound, setSound] = useState(null);
+
+    const [sounds, setSounds] = useState([sound1, sound2])
+    const [soundURI, setSoundURI] = useState(sounds[0])
 
     // Load and play the audio
     async function playSound() {
         if (sound === null) {
             // Load the sound for the first time
             const { sound: playbackObject } = await Audio.Sound.createAsync(
-                require('./assets/sampleAudioForReactnative.m4a')
+                soundURI, { shouldPlay: true }
             );
             setSound(playbackObject);
             await playbackObject.playAsync();  // Play the sound
@@ -52,11 +59,26 @@ export default function App() {
     return (
         <View style={styles.container}>
             <Text>React Native Audio Playback App</Text>
+
+            <View style={styles.container}>
+                <Picker
+                    selectedValue={soundURI}
+                    onValueChange={(itemValue) => setSoundURI(itemValue)}
+                >
+                    <Picker.Item label="Test sound" value={sounds[0]} />
+                    <Picker.Item label="NCS Song" value={sounds[1]} />
+
+                </Picker>
+            </View>
+
+
             <TouchableOpacity onPress={playSound}>
                 <Image
                     source={require('./assets/play.png')}  // Local image file
                     style={styles.buttonImage}
                 />
+
+                <Text style={styles.text}>Play</Text>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={pauseSound}>
@@ -64,6 +86,8 @@ export default function App() {
                     source={require('./assets/pause.png')}  // Local image file
                     style={styles.buttonImage}
                 />
+
+                <Text style={styles.text}>Pause</Text>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={stopSound}>
@@ -71,10 +95,12 @@ export default function App() {
                     source={require('./assets/stop.png')}  // Local image file
                     style={styles.buttonImage}
                 />
+
+                <Text style={styles.text}>Stop</Text>
             </TouchableOpacity>
 
             <StatusBar style="auto" />
-        </View>
+        </View >
     );
 }
 
@@ -89,5 +115,10 @@ const styles = StyleSheet.create({
         width: 50,
         height: 50,
         margin: 50,
+    },
+    text: {
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 });
